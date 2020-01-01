46b81019cf66e673f14329004abc72604c455393
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-01 15:48:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,12 +42,16 @@ end
 
 def update_counting_hash(hash, key)
 
+  hash = {
+    key =>
+  }
+
   if hash[key]
     hash[key] += 1
 
   else
-    
-    hash[key] = 0
+
+    hash[key] = 1
   end
 
 end
